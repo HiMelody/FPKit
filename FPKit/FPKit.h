4faf88a67ceb65eadd89f11b6d63c8c39e1c8b26
FPLiang <liang.soul@gmail.com>
2021-11-26 10:45:04 +0800

    初始化

---
+++ FPKit/FPKit.h
@@ -0,0 +1,18 @@
+//
+//  FPKit.h
+//  FPKit
+//
+//  Created by GB on 2021/11/26.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FPKit.
+FOUNDATION_EXPORT double FPKitVersionNumber;
+
+//! Project version string for FPKit.
+FOUNDATION_EXPORT const unsigned char FPKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FPKit/PublicHeader.h>
+
+
